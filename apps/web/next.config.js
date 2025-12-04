0378656155c7e46772ef0b7db65a1b4c1dd2a1d2
Allan <allankyagulanyi8@gmail.com>
2025-12-04 16:26:28 +0300

    Fix Vercel deployment: resolve dependencies and API routes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -29,6 +29,9 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     // External packages that don't need to be bundled
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
+
+     // Fix Privy Solana module resolution
+    config.resolve.alias['@solana-program/system'] = '@solana/web3.js';
     
     // Optimize bundle size
     if (!isServer) {
